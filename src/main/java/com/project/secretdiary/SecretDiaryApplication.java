3d1f4b6b87c8547c2889cbd7e85533b00a3a864a
coucouluv <oy5325@gmail.com>
2023-03-02 02:32:25 +0900

    feat: 개발 환경 구축

---
+++ src/main/java/com/project/secretdiary/SecretDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.project.secretdiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecretDiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecretDiaryApplication.class, args);
+    }
+
+}
