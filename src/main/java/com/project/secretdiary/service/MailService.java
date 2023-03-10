f1a34ff350ce9971ec6d3e009fe74a1020f5d5bf
coucouluv <oy5325@gmail.com>
2023-03-10 15:29:41 +0900

    feat: 메일 service 구현

---
+++ src/main/java/com/project/secretdiary/service/MailService.java
@@ -0,0 +1,25 @@
+package com.project.secretdiary.service;
+
+import com.project.secretdiary.dto.request.MailRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MailService {
+    private final JavaMailSender mailSender;
+    @Value("${spring.mail.username}")
+    private String adminEmail;
+    
+    public void sendMail(final MailRequest mailRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(mailRequest.getAddress());
+        message.setFrom(adminEmail);
+        message.setSubject(mailRequest.getTitle());
+        message.setText(mailRequest.getMessage());
+        mailSender.send(message);
+    }
+}
