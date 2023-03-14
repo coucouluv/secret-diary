c34e69225325be8b057b214f800fec0a98906448
coucouluv <oy5325@gmail.com>
2023-03-14 19:56:50 +0900

    feat: 멤버 정보 응답 dto 구현

---
+++ src/main/java/com/project/secretdiary/dto/response/MemberResponse.java
@@ -0,0 +1,29 @@
+package com.project.secretdiary.dto.response;
+
+import com.project.secretdiary.entity.MemberEntity;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class MemberResponse {
+    private String userId;
+    private String name;
+    private String email;
+    private String statusMessage;
+    private String url;
+
+    private MemberResponse(final String userId, final String name, final String email,
+                          final String statusMessage, final String url) {
+        this.userId = userId;
+        this.name = name;
+        this.email = email;
+        this.statusMessage = statusMessage;
+        this.url = url;
+    }
+
+    public static MemberResponse of(final MemberEntity member, final String preSignedUrl) {
+        return new MemberResponse(member.getUserId(), member.getName(), member.getEmail(),
+                member.getStatusMessage(), preSignedUrl);
+    }
+}
