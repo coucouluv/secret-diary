ebbf02d67bcf60a86ce0ed114b2c86277abe6121
coucouluv <oy5325@gmail.com>
2023-03-18 17:07:35 +0900

    feat: 다이어리 수정 dto 구현

---
+++ src/main/java/com/project/secretdiary/dto/request/DiaryUpdateRequest.java
@@ -0,0 +1,26 @@
+package com.project.secretdiary.dto.request;
+
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class DiaryUpdateRequest {
+
+    @NotNull(message = "다이어리 아이디를 입력하세요.")
+    private Long diaryId;
+
+    @NotBlank(message = "제목을 입력하세요.")
+    private String title;
+
+    @NotBlank(message = "내용을 입력하세요.")
+    private String text;
+
+    private String url;
+
+
+}
