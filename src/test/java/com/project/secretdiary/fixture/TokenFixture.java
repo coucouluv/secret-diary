c37b6885b45d301dc5715936784de81f4d798d7c
coucouluv <oy5325@gmail.com>
2023-03-20 21:45:00 +0900

    test: 토큰 테스트 픽스처 구현

---
+++ src/test/java/com/project/secretdiary/fixture/TokenFixture.java
@@ -0,0 +1,14 @@
+package com.project.secretdiary.fixture;
+
+import com.project.secretdiary.dto.response.TokenResponse;
+
+public class TokenFixture {
+
+    public static String 액세스_토큰 = "access";
+    public static String 리프레시_토큰 = "access";
+    public static long 유효_기간 = 1000;
+
+    public static TokenResponse 토큰_응답() {
+        return new TokenResponse(액세스_토큰, 리프레시_토큰, 유효_기간);
+    }
+}
