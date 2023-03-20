e32b3c889ea39c200c944a2406a27215262409d3
coucouluv <oy5325@gmail.com>
2023-03-20 22:02:29 +0900

    test: applicationTest 파일 생성

---
+++ src/test/java/com/project/secretdiary/SecretDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.secretdiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecretDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
