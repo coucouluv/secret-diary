8956a65f366a3c0a6d10f2d4d92a239d2b0bbc1c
coucouluv <oy5325@gmail.com>
2023-03-20 21:42:13 +0900

    test: 가독성을 위해 멤버 테스트 피처 수정

--- src/test/java/com/project/secretdiary/fixture/MemberFixture.java
+++ src/test/java/com/project/secretdiary/fixture/MemberFixture.java
@@ -4,47 +4,53 @@ import com.project.secretdiary.dto.request.MemberRequest;
 import com.project.secretdiary.entity.MemberEntity;
 
 public class MemberFixture {
-    private static final Long ID = 1L;
-    private static final Long ID_FOR_FRIEND = 2L;
-    private static final String USER_ID = "회원";
-    private static final String FRIEND_ID = "친구";
+    public static final Long 아이디 = 1L;
+    public static final Long 친구_아이디 = 2L;
+    public static final String 회원_로그인_아이디 = "userid";
+    public static final String 친구_로그인_아이디 = "friendid";
 
-    private static final String USER_NAME = "회원이름";
-    private static final String FRIEND_NAME = "친구이름";
+    public static final String 회원_이름 = "회원이름";
+    public static final String 친구_이름 = "친구이름";
 
-    private static final String USER_EMAIL = "123@naver.com";
-    private static final String FRIEND_EMAIL = "456@naver.com";
+    public static final String 회원_이메일 = "123@naver.com";
+    public static final String 친구_이메일 = "456@naver.com";
 
-    private static final String PASS_WORD = "test";
+    public static final String 비밀번호 = "test1234";
+
+    public static final String 상태메세지 = "message";
+    public static final String 이미지 = "url";
 
     public static MemberEntity 회원() {
         return MemberEntity.builder()
-                .id(ID)
-                .userId(USER_ID)
-                .password(PASS_WORD) //비밀번호 암호화
-                .name(USER_NAME)
-                .email(USER_EMAIL)
+                .id(아이디)
+                .userId(회원_로그인_아이디)
+                .password(비밀번호)
+                .name(회원_이름)
+                .email(회원_이메일)
+                .statusMessage(상태메세지)
+                .url(이미지)
                 .build();
 
     }
 
     public static MemberEntity 친구() {
         return MemberEntity.builder()
-                .id(ID_FOR_FRIEND)
-                .userId(FRIEND_ID)
-                .password(null) //비밀번호 암호화
-                .name(FRIEND_NAME)
-                .email(FRIEND_EMAIL)
+                .id(친구_아이디)
+                .userId(친구_로그인_아이디)
+                .password(null)
+                .name(친구_이름)
+                .email(친구_이메일)
                 .build();
 
     }
 
+    public static MemberRequest 회원가입_요청() {
+        return new MemberRequest(아이디, 회원_로그인_아이디, 회원_이름, 회원_이메일, 비밀번호);
+    }
+
     public static MemberRequest 친구_DTO() {
-        return MemberRequest.builder()
-                .id(ID_FOR_FRIEND)
-                .userId(FRIEND_ID)
-                .email(FRIEND_EMAIL)
-                .name(FRIEND_NAME).build();
+        return new MemberRequest(친구_아이디, 친구_로그인_아이디, 친구_이름, 친구_이메일, null);
+
     }
 
 }
