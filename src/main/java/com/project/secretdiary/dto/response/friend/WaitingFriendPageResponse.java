03a70fef52de34ccb4f692877d1ba82607adc610
coucouluv <oy5325@gmail.com>
2023-03-30 14:31:41 +0900

    feat: 요청된 친구 조회 페이징 dto 구현

---
+++ src/main/java/com/project/secretdiary/dto/response/friend/WaitingFriendPageResponse.java
@@ -0,0 +1,27 @@
+package com.project.secretdiary.dto.response.friend;
+
+import com.project.secretdiary.entity.MemberEntity;
+import lombok.Getter;
+import org.springframework.data.domain.Slice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class WaitingFriendPageResponse {
+    private boolean hasNext;
+    List<WaitingFriendResponse> friends;
+
+    private WaitingFriendPageResponse(final boolean hasNext, final List<WaitingFriendResponse> friends) {
+        this.hasNext = hasNext;
+        this.friends = friends;
+    }
+
+    public static WaitingFriendPageResponse from(final Slice<MemberEntity> slice) {
+        List<WaitingFriendResponse> friendResponses = slice.getContent()
+                .stream()
+                .map(WaitingFriendResponse::from)
+                .collect(Collectors.toList());
+        return new WaitingFriendPageResponse(slice.hasNext(), friendResponses);
+    }
+}
