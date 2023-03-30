7f1b09d93c01afff84e18d28186e8af5e43e2a2b
coucouluv <oy5325@gmail.com>
2023-03-30 14:30:22 +0900

    feat: 요청된 친구 조회 쿼리, 친구 조회 쿼리 구현

--- src/main/java/com/project/secretdiary/repository/FriendRepository.java
+++ src/main/java/com/project/secretdiary/repository/FriendRepository.java
@@ -1,17 +1,24 @@
 package com.project.secretdiary.repository;
 
-import com.project.secretdiary.entity.FriendEntity;
+import com.project.secretdiary.entity.Friend;
 import com.project.secretdiary.entity.MemberEntity;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface FriendRepository extends JpaRepository<FriendEntity, Long> {
-    List<FriendEntity> findByMember(MemberEntity member);
-    List<FriendEntity> findByFriend(MemberEntity friend);
-    Optional<FriendEntity> findByMemberAndFriend(MemberEntity member, MemberEntity friend);
+public interface FriendRepository extends JpaRepository<Friend, Long> {
+    @Query("select f.friend from Friend f where f.member.id = :memberId and f.friendStatus = 'COMPLETED'")
+    Slice<MemberEntity> findFriendWithComplete(@Param("memberId") Long memberId, Pageable pageable);
+
+    @Query("select f.member from Friend f where f.friend.id = :friendId and f.friendStatus = 'WAITING'")
+    Slice<MemberEntity> findFriendWithWaiting(@Param("friendId") Long friendId, Pageable pageable);
+
+    Optional<Friend> findByMemberAndFriend(MemberEntity member, MemberEntity friend);
     boolean existsByMemberAndFriend(MemberEntity member, MemberEntity friend);
 }
