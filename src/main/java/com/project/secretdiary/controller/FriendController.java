a654515ff4548944c94740ce013d1a07baf8dc06
coucouluv <oy5325@gmail.com>
2023-05-20 18:16:39 +0900

    feat: 친구 초대 api 구현

--- src/main/java/com/project/secretdiary/controller/FriendController.java
+++ src/main/java/com/project/secretdiary/controller/FriendController.java
@@ -1,6 +1,7 @@
 package com.project.secretdiary.controller;
 
 import com.project.secretdiary.dto.LoginMember;
+import com.project.secretdiary.dto.request.friend.InvitationRequest;
 import com.project.secretdiary.dto.response.friend.FriendPageResponse;
 import com.project.secretdiary.dto.response.friend.WaitingFriendPageResponse;
 import com.project.secretdiary.service.FriendService;
@@ -8,6 +9,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,35 +27,43 @@ public class FriendController {
     }
 
     @PostMapping("/accept/{id}")
-    public ResponseEntity<Void> acceptFriend(@CurrentUser LoginMember loginMember,
+    public ResponseEntity<Void> acceptFriend(@CurrentUser final LoginMember loginMember,
                                        @PathVariable("id") final String friendId) {
         friendService.accept(loginMember.getId(), friendId);
         return ResponseEntity.ok().build();
     }
 
     @PostMapping("/deny/{id}")
-    public ResponseEntity<Void> denyFriend(@CurrentUser LoginMember loginMember,
+    public ResponseEntity<Void> denyFriend(@CurrentUser final LoginMember loginMember,
                                      @PathVariable("id") final String friendId) {
         friendService.deny(loginMember.getId(), friendId);
         return ResponseEntity.ok().build();
     }
 
     @GetMapping
-    public ResponseEntity<FriendPageResponse> getFriends(@CurrentUser LoginMember loginMember, Pageable pageable) {
+    public ResponseEntity<FriendPageResponse> getFriends(@CurrentUser final LoginMember loginMember,
+                                                         final Pageable pageable) {
 
         return ResponseEntity.ok(friendService.findFriends(loginMember.getId(), pageable));
     }
 
     @GetMapping("/waiting")
-    public ResponseEntity<WaitingFriendPageResponse> getWaiting(@CurrentUser LoginMember loginMember,
-                                                                Pageable pageable) {
+    public ResponseEntity<WaitingFriendPageResponse> getWaiting(@CurrentUser final LoginMember loginMember,
+                                                                final Pageable pageable) {
         return ResponseEntity.ok( friendService.findFriendsWithWaiting(loginMember.getId(), pageable));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteFriend(@CurrentUser LoginMember loginMember,
+    public ResponseEntity<Void> deleteFriend(@CurrentUser final LoginMember loginMember,
                                        @PathVariable("id") final String friendId) {
         friendService.delete(loginMember.getId(), friendId);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/invitation")
+    public ResponseEntity<Void> inviteFriend(@CurrentUser final LoginMember loginMember,
+                                             @RequestBody @Valid final InvitationRequest invitationRequest) {
+        friendService.invite(loginMember.getId(), invitationRequest.getEmail());
         return ResponseEntity.ok().build();
     }
 }
