b7128f1a45d6bafdfb43fabd771e2d0a73741726
coucouluv <oy5325@gmail.com>
2023-05-21 00:14:00 +0900

    refactor: findById 에서 member 패치 조인 적용

--- src/main/java/com/project/secretdiary/repository/DiaryRepository.java
+++ src/main/java/com/project/secretdiary/repository/DiaryRepository.java
@@ -2,9 +2,16 @@ package com.project.secretdiary.repository;
 
 import com.project.secretdiary.entity.Diary;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface DiaryRepository extends JpaRepository<Diary, Long> {
+
+    @Query("select d from Diary d join fetch d.member where d.id = :id")
+    Optional<Diary> findById(@Param("id") Long id);
 }
 
