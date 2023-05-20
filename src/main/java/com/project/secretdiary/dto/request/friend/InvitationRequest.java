e110382839622c69961e4813eecbc745c7c0a6fc
coucouluv <oy5325@gmail.com>
2023-05-20 18:16:58 +0900

    feat: 친구 초대 dto 구현

---
+++ src/main/java/com/project/secretdiary/dto/request/friend/InvitationRequest.java
@@ -0,0 +1,19 @@
+package com.project.secretdiary.dto.request.friend;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InvitationRequest {
+
+    @NotBlank(message = "이메일을 입력하세요.")
+    @Email(message = "이메일 형식에 맞게 입력하세요.")
+    private String email;
+
+}
